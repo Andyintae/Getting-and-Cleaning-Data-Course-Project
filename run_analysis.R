452c1073cf9f172c6acfa71f574f5fd7a4ab6235
Andyintae <runrunintae@gmail.com>
2020-06-07 00:15:50 +0900

    ongoing_2

--- run_analysis.R
+++ run_analysis.R
@@ -7,9 +7,7 @@
 library(tidyverse)
 
 
-
 # 2. Create and Set work directory ------------------------------------------------------
-
 
 if(!file.exists("project")){dir.create("project")}
 
@@ -26,38 +24,50 @@ download.file(fileUrl, destfile = paste0(wd,"/20Dataset.zip"))
 unzip(zipfile = "20Dataset.zip")
 
 
+# 4. Read and Merge data ----------------------------------------------------
 
-# Read and Merge files ----------------------------------------------------
+dir <- "./UCI HAR Dataset"
 
-dir <- file.path("./UCI HAR Dataset")
-
-activity_lab  <- read.table(paste0(dir,"/activity_labels.txt"))
-names()
-
-features      <- read.table(paste0(dir,"/features.txt"))
-
+ # 4-1. subject    
 subject_train <- read.table(paste0(dir,"/train/subject_train.txt"))
 subject_test  <- read.table(paste0(dir,"/test/subject_test.txt"))
 subject_merge <- bind_rows(subject_train, subject_test)
+
 colnames(subject_merge) <- "subject"
 
+ # 4-2. y
 y_train       <- read.table(paste0(dir,"/train/Y_train.txt"))
 y_test        <- read.table(paste0(dir,"/test/Y_test.txt"))
 y_merge       <- bind_rows(y_train, y_test)
-colnames(y_merge) <- "labels"
 
+colnames(y_merge) <- "activity_id"
+activity_labels   <- read.table(paste0(dir,"/activity_labels.txt"))
+colnames(activity_labels) <- c("activity_id", "activity")
+y_merge_label             <- y_merge %>% left_join(activity_labels, by = "activity_id")
+
+ # 4-3. x
 x_train       <- read.table(paste0(dir,"/train/X_train.txt"))
 x_test        <- read.table(paste0(dir,"/test/X_test.txt"))
 x_merge       <- bind_rows(x_train, x_test)
+
+features          <- read.table(paste0(dir,"/features.txt"))
 colnames(x_merge) <- features[,2]
 
+ # 4-4. Merge 
+dim(subject_mrge)
+dim(y_merge_label)
+dim(x_merge)
 
-dim(activity_lab)
-dim(features)
-dim(subject_train)
-dim(x_train)
-dim(y_train)
-dim(y_train)
-dim(subject_test)
-dim(x_test)
-dim(y_test)
+merged <- bind_cols(subject_merge, y_merge_label, x_merge)
+
+view(merged)
+
+
+# 5. tidy data ---------------------------------------------------
+
+tidy_data <- merged %>% 
+          select(subject, activity, contains(c("mean()","std()"))) %>% 
+          gather(features, value, -c(subject, activity)) %>% 
+          separate(col = features, into = c("feature", "descriptive", "direction"), sep = "-")
+
+
